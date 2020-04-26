33c9244634ac2bc52d8cc8bb5caf637d587a8ba2
alfanor <Sinistre>
2020-04-26 11:39:50 +0200

    Simplification BDD

--- Artefacts/Configuration/SQL.php
+++ Artefacts/Configuration/SQL.php
@@ -1,13 +1,5 @@
 <?php
-if(long2ip(ip2long($_SERVER['REMOTE_ADDR'])) == '127.0.0.1') {
-	$dsn = 'mysql:dbname=travian_artefact;host=127.0.0.1';
-	$user = 'travian_artefact';
-	$password = 'travian_artefact';
-}
-
-else {
-	$dsn = 'mysql:dbname=dbname;host=127.0.0.1';
-	$user = 'user';
-	$password = 'password';
-}
+$dsn = 'mysql:dbname=travian_artefact;host=127.0.0.1';
+$user = 'spartan';
+$password = '548(-5(lokpoçà]-ç1';
 ?>
